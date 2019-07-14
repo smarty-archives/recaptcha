0dfa0cdde7de68466d8c090410fb34ae75545e7c
Jonathan Oliver <jonathan.s.oliver42@gmail.com>
2019-07-14 00:03:43 -0600

    Behavior for DefaultVerifier.

---
+++ default_verifier.go
@@ -0,0 +1,111 @@
+package checkpoint
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+type DefaultVerifier struct {
+	secret    func() string
+	client    httpClient
+	endpoint  string
+	threshold float32
+	hosts     map[string]struct{}
+	actions   map[string]struct{}
+}
+
+func NewVerifier(options ...VerifierOption) *DefaultVerifier {
+	this := &DefaultVerifier{}
+
+	WithSecret(func() string { return "" })(this)
+	WithHTTPClient(http.DefaultClient)(this)
+	WithEndpoint(defaultEndpoint)(this)
+	WithRequiredThreshold(defaultThreshold)(this)
+	WithAllowedHosts()(this)
+	WithAllowedActions()(this)
+
+	for _, option := range options {
+		option(this)
+	}
+
+	return this
+}
+
+func (this *DefaultVerifier) Verify(token, clientIP string) (bool, error) {
+	token = strings.TrimSpace(token)
+	if len(token) == 0 {
+		return false, nil
+	} else {
+		return this.verify(token, clientIP)
+	}
+}
+func (this *DefaultVerifier) verify(token, clientIP string) (bool, error) {
+	if response, err := this.newRequest(token, clientIP); err != nil {
+		return false, ErrLookupFailure
+	} else if lookup, err := this.parseLookup(response); err != nil {
+		return false, ErrLookupFailure
+	} else {
+		return lookup.IsValid(this.hosts, this.actions, this.threshold)
+	}
+}
+func (this *DefaultVerifier) newRequest(token, clientIP string) (*http.Response, error) {
+	request, _ := http.NewRequest(http.MethodPost, this.endpoint, nil)
+	request.Form = url.Values{
+		"secret":   []string{this.secret()},
+		"response": []string{token},
+	}
+
+	if len(clientIP) > 0 {
+		request.Form.Set("remoteip", clientIP)
+	}
+
+	return this.client.Do(request)
+}
+func (this *DefaultVerifier) parseLookup(response *http.Response) (lookup defaultLookup, err error) {
+	defer func() { _ = response.Body.Close() }()
+	err = json.NewDecoder(response.Body).Decode(&lookup)
+	return lookup, err
+}
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+type VerifierOption func(this *DefaultVerifier)
+
+func WithSecret(callback func() string) VerifierOption {
+	return func(this *DefaultVerifier) { this.secret = callback }
+}
+func WithHTTPClient(value httpClient) VerifierOption {
+	return func(this *DefaultVerifier) { this.client = value }
+}
+func WithEndpoint(value string) VerifierOption {
+	return func(this *DefaultVerifier) { this.endpoint = value }
+}
+func WithRequiredThreshold(value float32) VerifierOption {
+	return func(this *DefaultVerifier) { this.threshold = value }
+}
+func WithAllowedHosts(values ...string) VerifierOption {
+	return func(this *DefaultVerifier) { this.hosts = createMap(values) }
+}
+func WithAllowedActions(values ...string) VerifierOption {
+	return func(this *DefaultVerifier) { this.actions = createMap(values) }
+}
+func createMap(values []string) map[string]struct{} {
+	allowed := make(map[string]struct{}, len(values))
+	for _, value := range values {
+		allowed[value] = struct{}{}
+	}
+	return allowed
+}
+
+const (
+	defaultEndpoint  = "https://www.google.com/recaptcha/api/siteverify"
+	defaultThreshold = 0.3
+)
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+type httpClient interface {
+	Do(*http.Request) (*http.Response, error)
+}
---
+++ default_verifier_test.go
@@ -0,0 +1,146 @@
+package checkpoint
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
+)
+
+func TestDefaultVerifierFixture(t *testing.T) {
+	gunit.Run(new(DefaultVerifierFixture), t)
+}
+
+type DefaultVerifierFixture struct {
+	*gunit.Fixture
+
+	verifier *DefaultVerifier
+
+	clientCalls          int
+	clientRequest        *http.Request
+	clientResponse       *http.Response
+	clientError          error
+	clientResponseBuffer *bytes.Buffer
+}
+
+func (this *DefaultVerifierFixture) Setup() {
+	this.clientResponseBuffer = bytes.NewBuffer(nil)
+	this.clientResponse = &http.Response{
+		Body: ioutil.NopCloser(this.clientResponseBuffer),
+	}
+
+	this.verifier = NewVerifier()
+	WithHTTPClient(this)(this.verifier)
+}
+
+func (this *DefaultVerifierFixture) TestEmptyTokenIsInvalid() {
+	result, err := this.verifier.Verify("", "")
+
+	this.So(result, should.BeFalse)
+	this.So(err, should.BeNil)
+}
+func (this *DefaultVerifierFixture) TestTokenLookupIsPerformed() {
+	WithSecret(func() string { return "my-secret" })(this.verifier)
+
+	_, _ = this.verifier.Verify(" token ", "client-ip")
+
+	this.So(this.clientCalls, should.Equal, 1)
+	this.So(this.clientRequest.Method, should.Equal, http.MethodPost)
+	this.So(this.clientRequest.URL.String(), should.Equal, defaultEndpoint)
+	this.So(this.clientRequest.Form, should.Resemble, url.Values{
+		"secret":   []string{"my-secret"},
+		"response": []string{"token"},
+		"remoteip": []string{"client-ip"},
+	})
+}
+func (this *DefaultVerifierFixture) TestCustomEndpoint() {
+	WithEndpoint("/custom-endpoint")(this.verifier)
+
+	_, _ = this.verifier.Verify("token", "")
+
+	this.So(this.clientCalls, should.Equal, 1)
+	this.So(this.clientRequest.URL.String(), should.Equal, "/custom-endpoint")
+}
+func (this *DefaultVerifierFixture) TestDoNotSendEmptyClientIP() {
+	WithSecret(func() string { return "my-secret" })(this.verifier)
+
+	_, _ = this.verifier.Verify("token", "")
+
+	this.So(this.clientRequest.Form, should.Resemble, url.Values{
+		"secret":   []string{"my-secret"},
+		"response": []string{"token"},
+	})
+}
+
+func (this *DefaultVerifierFixture) TestConnectivityError() {
+	this.clientError = errors.New("")
+
+	result, err := this.verifier.Verify("token", "ip")
+
+	this.So(result, should.BeFalse)
+	this.So(err, should.Equal, ErrLookupFailure)
+}
+func (this *DefaultVerifierFixture) TestParsingError() {
+	this.writeResponseBody("malformed json")
+
+	result, err := this.verifier.Verify("token", "ip")
+
+	this.So(result, should.BeFalse)
+	this.So(err, should.Equal, ErrLookupFailure)
+}
+
+func (this *DefaultVerifierFixture) TestValidLookup() {
+	this.writeResponseBody(`{"Score":1.0}`)
+
+	result, err := this.verifier.Verify("token", "ip")
+
+	this.So(result, should.BeTrue)
+	this.So(err, should.BeNil)
+}
+
+func (this *DefaultVerifierFixture) TestRequiredThreshold() {
+	this.writeResponseBody(`{}`)
+
+	WithRequiredThreshold(0.1)(this.verifier)
+
+	result, err := this.verifier.Verify("token", "ip")
+
+	this.So(result, should.BeFalse)
+	this.So(err, should.BeNil)
+}
+func (this *DefaultVerifierFixture) TestRequiredHostname() {
+	this.writeResponseBody(`{"Score":1.0}`)
+
+	WithAllowedHosts("hostname-required")(this.verifier)
+
+	result, err := this.verifier.Verify("token", "ip")
+
+	this.So(result, should.BeFalse)
+	this.So(err, should.BeNil)
+}
+func (this *DefaultVerifierFixture) TestRequiredAction() {
+	this.writeResponseBody(`{"Score":1.0}`)
+
+	WithAllowedActions("action-required")(this.verifier)
+
+	result, err := this.verifier.Verify("token", "ip")
+
+	this.So(result, should.BeFalse)
+	this.So(err, should.BeNil)
+}
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+func (this *DefaultVerifierFixture) Do(request *http.Request) (*http.Response, error) {
+	this.clientCalls++
+	this.clientRequest = request
+	return this.clientResponse, this.clientError
+}
+func (this *DefaultVerifierFixture) writeResponseBody(value string) {
+	this.clientResponseBuffer.WriteString(value)
+}
